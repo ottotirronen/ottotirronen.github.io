3ac2d92dabbb80f6386bbb0fc2779db6b3564836
ottotirronen <otto.tirronen@edu.karelia.fi>
2026-01-14 15:38:03 +0100

    sitetitle changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import preact from "@astrojs/preact";
 
 //https://astro.build/config
 export default defineConfig({
-  site: "https://ottotirronen.github.io",
+  site: "https://ottoalvar.com",
   integrations: [preact()],
 });
--- src/config.site.ts
+++ src/config.site.ts
@@ -1,4 +1,4 @@
 export const site = {
-  title: "ottotirronen",
-  description: "Exchange notes & dev logs",
+  title: "ottoalvar",
+  description: "Exchange notes & life logs",
 };
